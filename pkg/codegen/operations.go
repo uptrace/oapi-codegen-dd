fdd992044e28cf05d12cad7cc0ec3f6f4b6b90eb
Igor Khitryk <igor.khitryk@wolt.com>
2025-03-21 13:25:02 +0100

    Extract typedef parameters

--- pkg/codegen/operations.go
+++ pkg/codegen/operations.go
@@ -22,189 +22,15 @@ import (
 	"strconv"
 	"strings"
 	"text/template"
-	"unicode"
 
 	"github.com/getkin/kin-openapi/openapi3"
 
 	"github.com/doordash/oapi-codegen/v2/pkg/util"
 )
 
-type ParameterDefinition struct {
-	ParamName string // The original json parameter name, eg param_name
-	In        string // Where the parameter is defined - path, header, cookie, query
-	Required  bool   // Is this a required parameter?
-	Spec      *openapi3.Parameter
-	Schema    Schema
-}
-
-// TypeDef is here as an adapter after a large refactoring so that I don't
-// have to update all the templates. It returns the type definition for a parameter,
-// without the leading '*' for optional ones.
-func (pd ParameterDefinition) TypeDef() string {
-	typeDecl := pd.Schema.TypeDecl()
-	return typeDecl
-}
-
-// JsonTag generates the JSON annotation to map GoType to json type name. If Parameter
-// Foo is marshaled to json as "foo", this will create the annotation
-// 'json:"foo"'
-func (pd *ParameterDefinition) JsonTag() string {
-	if pd.Required {
-		return fmt.Sprintf("`json:\"%s\"`", pd.ParamName)
-	} else {
-		return fmt.Sprintf("`json:\"%s,omitempty\"`", pd.ParamName)
-	}
-}
-
-func (pd *ParameterDefinition) IsJson() bool {
-	p := pd.Spec
-	if len(p.Content) == 1 {
-		for k := range p.Content {
-			if util.IsMediaTypeJson(k) {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func (pd *ParameterDefinition) IsPassThrough() bool {
-	p := pd.Spec
-	if len(p.Content) > 1 {
-		return true
-	}
-	if len(p.Content) == 1 {
-		return !pd.IsJson()
-	}
-	return false
-}
-
-func (pd *ParameterDefinition) IsStyled() bool {
-	p := pd.Spec
-	return p.Schema != nil
-}
-
-func (pd *ParameterDefinition) Style() string {
-	style := pd.Spec.Style
-	if style == "" {
-		in := pd.Spec.In
-		switch in {
-		case "path", "header":
-			return "simple"
-		case "query", "cookie":
-			return "form"
-		default:
-			panic("unknown parameter format")
-		}
-	}
-	return style
-}
-
-func (pd *ParameterDefinition) Explode() bool {
-	if pd.Spec.Explode == nil {
-		in := pd.Spec.In
-		switch in {
-		case "path", "header":
-			return false
-		case "query", "cookie":
-			return true
-		default:
-			panic("unknown parameter format")
-		}
-	}
-	return *pd.Spec.Explode
-}
-
-func (pd ParameterDefinition) GoVariableName() string {
-	name := LowercaseFirstCharacters(pd.GoName())
-	if IsGoKeyword(name) {
-		name = "p" + UppercaseFirstCharacter(name)
-	}
-	if unicode.IsNumber([]rune(name)[0]) {
-		name = "n" + name
-	}
-	return name
-}
-
-func (pd ParameterDefinition) GoName() string {
-	goName := pd.ParamName
-	if extension, ok := pd.Spec.Extensions[extGoName]; ok {
-		if extGoFieldName, err := extParseGoFieldName(extension); err == nil {
-			goName = extGoFieldName
-		}
-	}
-	return SchemaNameToTypeName(goName)
-}
-
-func (pd ParameterDefinition) IndirectOptional() bool {
-	return !pd.Required && !pd.Schema.SkipOptionalPointer
-}
-
-type ParameterDefinitions []ParameterDefinition
-
-func (p ParameterDefinitions) FindByName(name string) *ParameterDefinition {
-	for _, param := range p {
-		if param.ParamName == name {
-			return &param
-		}
-	}
-	return nil
-}
-
-// DescribeParameters walks the given parameters dictionary, and generates the above
-// descriptors into a flat list. This makes it a lot easier to traverse the
-// data in the template engine.
-func DescribeParameters(params openapi3.Parameters, path []string) ([]ParameterDefinition, error) {
-	outParams := make([]ParameterDefinition, 0)
-	for _, paramOrRef := range params {
-		param := paramOrRef.Value
-
-		goType, err := paramToGoType(param, append(path, param.Name))
-		if err != nil {
-			return nil, fmt.Errorf("error generating type for param (%s): %s",
-				param.Name, err)
-		}
-
-		pd := ParameterDefinition{
-			ParamName: param.Name,
-			In:        param.In,
-			Required:  param.Required,
-			Spec:      param,
-			Schema:    goType,
-		}
-
-		// If this is a reference to a predefined type, simply use the reference
-		// name as the type. $ref: "#/components/schemas/custom_type" becomes
-		// "CustomType".
-		if IsGoTypeReference(paramOrRef.Ref) {
-			goType, err := RefPathToGoType(paramOrRef.Ref)
-			if err != nil {
-				return nil, fmt.Errorf("error dereferencing (%s) for param (%s): %s",
-					paramOrRef.Ref, param.Name, err)
-			}
-			pd.Schema.GoType = goType
-		}
-		outParams = append(outParams, pd)
-	}
-	return outParams, nil
-}
-
 type SecurityDefinition struct {
 	ProviderName string
 	Scopes       []string
-}
-
-func DescribeSecurityDefinition(securityRequirements openapi3.SecurityRequirements) []SecurityDefinition {
-	outDefs := make([]SecurityDefinition, 0)
-
-	for _, sr := range securityRequirements {
-		for _, k := range SortedMapKeys(sr) {
-			v := sr[k]
-			outDefs = append(outDefs, SecurityDefinition{ProviderName: k, Scopes: v})
-		}
-	}
-
-	return outDefs
 }
 
 // OperationDefinition describes an Operation
@@ -622,20 +448,6 @@ func OperationDefinitions(swagger *openapi3.T, initialismOverrides bool) ([]Oper
 				Bodies:          bodyDefinitions,
 				Responses:       responseDefinitions,
 				TypeDefinitions: typeDefinitions,
-			}
-
-			// check for overrides of SecurityDefinitions.
-			// See: "Step 2. Applying security:" from the spec:
-			// https://swagger.io/docs/specification/authentication/
-			if op.Security != nil {
-				opDef.SecurityDefinitions = DescribeSecurityDefinition(*op.Security)
-			} else {
-				// use global securityDefinitions
-				// globalSecurityDefinitions contains the top-level securityDefinitions.
-				// They are the default securityPermissions which are injected into each
-				// path, except for the case where a path explicitly overrides them.
-				opDef.SecurityDefinitions = DescribeSecurityDefinition(swagger.Security)
-
 			}
 
 			if op.RequestBody != nil {
